fb35de50cda4eacb1a24e093a9289d8915b8b271
Jack Danger Canty <jackdanger@squareup.com>
2013-01-06 15:40:52 -0800

    delivering email on signup

--- app/models/early_contact.rb
+++ app/models/early_contact.rb
@@ -3,4 +3,15 @@ class EarlyContact < ActiveRecord::Base
   attr_accessible :name, :email, :can_sponsor
 
   validates :email, :presence => true
+
+  after_save :deliver_email
+
+  private
+
+  def deliver_email
+    Mail::Message.new(subject: "TEDx pre-register",
+                      body:    "Just signed up: #{name} #{email}",
+                      sender:  "noreply@tedxfidiwomen.com",
+                      to:      ENV['contact_emails'].split(',')).deliver
+  end
 end
